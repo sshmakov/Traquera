95a58e08f134861e6ec2f96bb0660142004a88ad
shmakov <sergey-shmakov@yandex.ru>
2016-06-03 19:28:02 +0300

    К предыдущей ревизии

---
+++ jira/jirausermodel.h
@@ -0,0 +1,27 @@
+#ifndef JIRAUSERMODEL_H
+#define JIRAUSERMODEL_H
+
+#include <QAbstractListModel>
+
+class JiraProject;
+class JiraUserModelPrivate;
+
+class JiraUserModel : public QAbstractListModel
+{
+    Q_OBJECT
+private:
+    JiraUserModelPrivate *d;
+public:
+    explicit JiraUserModel(JiraProject *project);
+    ~JiraUserModel();
+    QVariant data(const QModelIndex &index, int role) const;
+    int rowCount(const QModelIndex &parent) const;
+public slots:
+    void refresh(const QString &firstChars);
+signals:
+    
+public slots:
+    
+};
+
+#endif // JIRAUSERMODEL_H
---
+++ tqplugapi/tqplug.cpp
@@ -0,0 +1,140 @@
+#include "tqplug.h"
+
+class TQBaseRecordTypeDefPrivate
+{
+    TQAbstractProject *prj;
+    friend class TQBaseRecordTypeDef;
+};
+
+TQBaseRecordTypeDef::TQBaseRecordTypeDef(TQAbstractProject *prj) :
+    d(new TQBaseRecordTypeDefPrivate())
+{
+    d->prj = prj;
+}
+
+TQBaseRecordTypeDef::~TQBaseRecordTypeDef()
+{
+}
+
+TQAbstractFieldType TQBaseRecordTypeDef::getFieldType(int vid, bool *ok) const
+{
+    if(ok)
+        *ok = containFieldVid(vid);
+    return TQAbstractFieldType(this, vid);
+}
+
+TQAbstractFieldType TQBaseRecordTypeDef::getFieldType(const QString &name, bool *ok) const
+{
+    int vid = fieldVid(name);
+    if(vid == TQ::TQ_NO_VID)
+    {
+        if(ok)
+            ok = false;
+        return TQAbstractFieldType(this, vid);
+    }
+    return getFieldType(vid, ok);
+}
+
+bool TQBaseRecordTypeDef::containFieldVid(int vid) const
+{
+    QList<int> vids = fieldVids();
+    return vids.contains(vid);
+}
+
+QString TQBaseRecordTypeDef::fieldSystemName(int vid) const
+{
+    return fieldName(vid);
+}
+
+bool TQBaseRecordTypeDef::canFieldSubmit(int vid) const
+{
+    Q_UNUSED(vid)
+    return true;
+}
+
+bool TQBaseRecordTypeDef::canFieldUpdate(int vid) const
+{
+    Q_UNUSED(vid)
+    return true;
+}
+
+bool TQBaseRecordTypeDef::isNullable(int vid) const
+{
+    Q_UNUSED(vid)
+    return true;
+}
+
+bool TQBaseRecordTypeDef::hasChoiceList(int vid) const
+{
+    Q_UNUSED(vid)
+    return false;
+}
+
+TQChoiceList TQBaseRecordTypeDef::choiceTable(const QString &tableName) const
+{
+    Q_UNUSED(tableName)
+    return TQChoiceList();
+}
+
+QIODevice *TQBaseRecordTypeDef::defineSource() const
+{
+    return 0;
+}
+
+QString TQBaseRecordTypeDef::dateTimeFormat() const
+{
+    QLocale locale = QLocale::system();
+    return locale.dateFormat(QLocale::ShortFormat)+" "+locale.timeFormat(QLocale::LongFormat);
+//    return QLocale::system().dateTimeFormat(QLocale::ShortFormat);
+}
+
+QString TQBaseRecordTypeDef::valueToDisplay(int vid, const QVariant &value) const
+{
+    if(value.type() == QVariant::DateTime)
+        return value.toDateTime().toString(dateTimeFormat());
+    return value.toString();
+}
+
+QVariant TQBaseRecordTypeDef::displayToValue(int vid, const QString &text) const
+{
+    return text;
+}
+
+TQAbstractProject *TQBaseRecordTypeDef::project() const
+{
+    return d->prj;
+}
+
+bool TQBaseRecordTypeDef::hasFieldCustomEditor(int vid) const
+{
+    Q_UNUSED(vid)
+    return false;
+}
+
+QWidget *TQBaseRecordTypeDef::createCustomEditor(int vid, QWidget *parent) const
+{
+    Q_UNUSED(vid)
+    Q_UNUSED(parent)
+    return 0;
+}
+
+QStringList TQBaseRecordTypeDef::noteTitleList() const
+{
+    return QStringList();
+}
+
+
+bool TQAbstractFieldType::hasCustomFieldEditor() const
+{
+    if(isValid())
+        return def->hasFieldCustomEditor(vid);
+    return false;
+}
+
+QWidget *TQAbstractFieldType::createFieldEditor(QWidget *parent) const
+{
+    if(isValid())
+        return def->createCustomEditor(vid, parent);
+    return 0;
+}
+
