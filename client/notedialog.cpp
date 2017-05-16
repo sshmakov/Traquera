7d1212a64fbd7bb901e58117ab9d3d76d96d882b
shmakov <sergey-shmakov@yandex.ru>
2017-05-16 23:31:35 +0300

    Функция показа/скрытия поля с заголовком комментария

--- client/notedialog.cpp
+++ client/notedialog.cpp
@@ -13,6 +13,11 @@ NoteDialog::~NoteDialog()
     //delete ui;
 }
 
+void NoteDialog::setNoteTitleVisible(bool value)
+{
+    titleEdit->setVisible(value);
+}
+
 void NoteDialog::on_actionAddNote_triggered()
 {
     accept();
--- client/notedialog.h
+++ client/notedialog.h
@@ -11,10 +11,12 @@ class NoteDialog;
 class NoteDialog : public QDialog, public Ui::NoteDialog
 {
     Q_OBJECT
-    
+    Q_PROPERTY(bool noteTitleVisible WRITE setNoteTitleVisible)
 public:
     explicit NoteDialog(QWidget *parent = 0);
     ~NoteDialog();
+    void setNoteTitleVisible(bool value);
+
 
 private slots:
     void on_actionAddNote_triggered();
